efb3b8e0ece3fa3c98040f66d3140e44abb813e4
davor <davorzec.emedia@gmail.com>
2021-06-24 20:49:05 +0200

    remove unnecesery files in ScrapeController

--- app/Http/Controllers/ScrapeController.php
+++ app/Http/Controllers/ScrapeController.php
@@ -7,82 +7,10 @@ use Illuminate\Http\Request;
 
 class ScrapeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(Scrape $scrape)
     {
-        $scrapes_instance = new Scrape();
-        $scrapes = $scrapes_instance->getScrapes();
+        $scrapes = $scrape->getScrapes();
         
         return view('scrapes.index', ['scrapes' => $scrapes]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Scrape  $scrape
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Scrape $scrape)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Scrape  $scrape
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Scrape $scrape)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Scrape  $scrape
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Scrape $scrape)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Scrape  $scrape
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Scrape $scrape)
-    {
-        //
     }
 }

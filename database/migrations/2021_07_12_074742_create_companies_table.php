3317f90cdf4e6e4f1077a7caf8774643c455daee
davor <davorzec.emedia@gmail.com>
2021-07-12 09:51:03 +0200

    added company mcm

---
+++ database/migrations/2021_07_12_074742_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->integer('partner_id');
+            $table->string('company_name');
+            $table->string('vat_number');
+            $table->string('company_adress');
+            $table->integer('postal_code');
+            $table->string('town');
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}

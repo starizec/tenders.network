cc400f1eb66c825e3a79677f39504c959cfc3a6c
davor <davorzec.emedia@gmail.com>
2021-07-12 10:42:13 +0200

    click mm

---
+++ database/migrations/2021_07_12_075208_create_saves_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSavesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saves', function (Blueprint $table) {
+            $table->id();
+            $table->integer('partner_id');
+            $table->integer('user_id');
+            $table->bigInteger('tender_id');
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saves');
+    }
+}
